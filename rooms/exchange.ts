54957c5334d15903efca16976beb2ec5334f68f8
daily2409 <daily2409@gmail.com>
2020-06-18 17:25:17 +0700

    update account api

--- rooms/exchange.ts
+++ rooms/exchange.ts
@@ -1,25 +1,45 @@
 import  WebSocKet  from  "ws";
 import { Room } from "colyseus";
 import  moment  from "moment";
-import crypto from "crypto";
+// import crypto from "crypto";
+const crypto = require("crypto");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 // import XMLHttpRequest from "xmlhttprequest";
 export class ExchangeBinance extends Room{
-    CreateNewOrder(){
-        var url="https://api.binance.com";
-        var endpoint = "/api/v3/order";
-        var value = 
-        "symbol=TRXUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=0.1&price=0.01635&recvWindow=20000&timestamp="+Date.now();
+    AboutAccount(){
+        var  url = 'https://api.binance.com';
+        var endpoint = '/api/v3/account';
+        var dataquerystring = 'recWindow=20000&timestamp='+Date.now();
         var keys = {
-            "apiKey" : 'jIu7n0m09tp1qZCEha8vHoGlhWsFYd53U9n81bXwJsx0BbRF19mmVnLudA8UvQfp',
-            "secretKey" : 'x0SIhco6HtZJz9vaQRZWGABEH3vn8HbVW2Tyl96nfuZL3VHcZ0KJpvBpCpuj1Xgw'
+            'apiKey' : 'f2bO2GjACQOGM2VLQpjPFbccAjUBZRkA7uNWM40v563mIcofaiIP3qbMTyU7JBH7',
+            'secretKey' : 'ArCBx1AcUw9kJY7RYhnBMGdQDDd4grgkBzLg7D4uW0OypRVn1RN0SYRhZkNl9bER'
         }
-        var signature = crypto.createHmac('sha256', keys['secretKey']).update(value).digest("hex");
+        var signature = crypto.createHmac('sha256', keys['secretKey']).update(dataquerystring).digest('hex');
+        var urlAccount = url + endpoint + '?'+ dataquerystring + '&signature='+signature;
+        console.log(urlAccount);
+        var ourRequest = new XMLHttpRequest();
+        ourRequest.open('GET', urlAccount, true);
+        ourRequest.setRequestHeader('X-MBX-APIKEY', keys['apiKey']);
+        ourRequest.onload = function(){
+            console.log(ourRequest.responseText);
+        }
+        ourRequest.send();
+    }
+    CreateNewOrder(){
+        var url='https://api.binance.com';
+        var endpoint = '/api/v3/order';
+        var value = 'symbol=TRXUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=683.6&price=0.01609&recvWindow=20000&timestamp='+Date.now();
+        var keys = {
+            'apiKey' : 'f2bO2GjACQOGM2VLQpjPFbccAjUBZRkA7uNWM40v563mIcofaiIP3qbMTyU7JBH7',
+            'secretKey' : 'ArCBx1AcUw9kJY7RYhnBMGdQDDd4grgkBzLg7D4uW0OypRVn1RN0SYRhZkNl9bER'
+        }
+        var TradeURL = url+endpoint+'?'+value;
+        var signature = crypto.createHmac('sha256', keys['secretKey']).update(value).digest('hex');
         var Ourl = url + endpoint + '?' + value +  '&signature=' + signature;
         console.log(Ourl);
         var ourRequest = new XMLHttpRequest();
         ourRequest.open('POST', Ourl, true);
-        ourRequest.setRequestHeader('X-MBX-APIKEY', keys['secretKey']);
+        ourRequest.setRequestHeader('X-MBX-APIKEY', keys['apiKey']);
         ourRequest.onload = function(){
             console.log(ourRequest.responseText);
         }
@@ -27,7 +47,7 @@ export class ExchangeBinance extends Room{
         ourRequest.send();
     }
      GetTradeHistory(){
-         var url = "wss://stream.binance.com:9443/ws/ethbtc@aggTrade";
+         var url = "wss://stream.binance.com:9443/ws/trxusdt@aggTrade";
         const ws = new WebSocKet(url);
 
         
@@ -40,7 +60,7 @@ export class ExchangeBinance extends Room{
                 var time  =new Date(eventime).toLocaleDateString("en-US")+ " " + new Date(eventime).toLocaleTimeString("en-US") ;
                 this.broadcast("trade", time + " " + s + " " + price +  " " +parseFloat(qua));
                 
-            }
+            } 
         });
         
     }
@@ -80,8 +100,9 @@ export class ExchangeBinance extends Room{
     
     onCreate(option) {
         console.log("Chat room created!", option);
-        this.CreateNewOrder();
-        // this.GetTradeHistory();
+        // this.CreateNewOrder();
+        this.AboutAccount();
+        this.GetTradeHistory();
         // this.BookTicker();
         this.onMessage("message", (client, message)=>{
             
