1387ac1997dba75c595ed30d2f9189bf26eaa949
daily2409 <daily2409@gmail.com>
2020-06-20 12:02:01 +0700

    update api orders

--- rooms/exchange.ts
+++ rooms/exchange.ts
@@ -33,10 +33,35 @@ export class ExchangeBinance extends Room{
         ourRequest.send();
     }
 
+    QueryOrder(symbol, orderId){
+        var url='https://api.binance.com';
+        var endpoint = '/api/v3/allOrders';
+        var value = 'symbol='+symbol+'&orderId='+orderId+'&recvWindow=20000&timestamp='+Date.now();
+        // var value = 'symbol=TRXUSDT&side=SELL&type=LIMIT&timeInForce=GTC&quantity=699&price=0.016&recvWindow=20000&timestamp='+Date.now();
+        console.log(value);
+        var keys = {
+            'apiKey' : 'f2bO2GjACQOGM2VLQpjPFbccAjUBZRkA7uNWM40v563mIcofaiIP3qbMTyU7JBH7',
+            'secretKey' : 'ArCBx1AcUw9kJY7RYhnBMGdQDDd4grgkBzLg7D4uW0OypRVn1RN0SYRhZkNl9bER'
+        }
+        var TradeURL = url+endpoint+'?'+value;
+        var signature = crypto.createHmac('sha256', keys['secretKey']).update(value).digest('hex');
+        var Ourl = url + endpoint + '?' + value +  '&signature=' + signature;
+        console.log(Ourl);
+        var ourRequest = new XMLHttpRequest();
+        ourRequest.open('GET', Ourl, true);
+        ourRequest.setRequestHeader('X-MBX-APIKEY', keys['apiKey']);
+        ourRequest.onload = function(){
+            console.log(ourRequest.responseText);
+        }
+        
+        ourRequest.send();
+    }
     SellOrder(symbol, quanlity, price){
         var url='https://api.binance.com';
         var endpoint = '/api/v3/order';
         var value = 'symbol='+symbol+'&side=SELL&type=LIMIT&timeInForce=GTC&quantity='+quanlity+'&price='+price+'&recvWindow=20000&timestamp='+Date.now();
+        // var value = 'symbol=TRXUSDT&side=SELL&type=LIMIT&timeInForce=GTC&quantity=699&price=0.016&recvWindow=20000&timestamp='+Date.now();
+        console.log(value);
         var keys = {
             'apiKey' : 'f2bO2GjACQOGM2VLQpjPFbccAjUBZRkA7uNWM40v563mIcofaiIP3qbMTyU7JBH7',
             'secretKey' : 'ArCBx1AcUw9kJY7RYhnBMGdQDDd4grgkBzLg7D4uW0OypRVn1RN0SYRhZkNl9bER'
@@ -49,34 +74,14 @@ export class ExchangeBinance extends Room{
         ourRequest.open('POST', Ourl, true);
         ourRequest.setRequestHeader('X-MBX-APIKEY', keys['apiKey']);
         ourRequest.onload = function(){
-            console.log(ourRequest.responseText);
+            console.log(JSON.parse(ourRequest.responseText).orderId);
+            console.log(JSON.parse(ourRequest.responseText).clientOrderId);
         }
         
         ourRequest.send();
 
     }
-    CancelOrdeṛ(){
-        var url='https://api.binance.com';
-        var endpoint = '/api/v3/openOrders';
-        var value = 'symbol=TRXUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=683.6&price=0.01609&recvWindow=20000&timestamp='+Date.now();
-        var keys = {
-            'apiKey' : 'f2bO2GjACQOGM2VLQpjPFbccAjUBZRkA7uNWM40v563mIcofaiIP3qbMTyU7JBH7',
-            'secretKey' : 'ArCBx1AcUw9kJY7RYhnBMGdQDDd4grgkBzLg7D4uW0OypRVn1RN0SYRhZkNl9bER'
-        }
-        var TradeURL = url+endpoint+'?'+value;
-        var signature = crypto.createHmac('sha256', keys['secretKey']).update(value).digest('hex');
-        var Ourl = url + endpoint + '?' + value +  '&signature=' + signature;
-        console.log(Ourl);
-        var ourRequest = new XMLHttpRequest();
-        ourRequest.open('POST', Ourl, true);
-        ourRequest.setRequestHeader('X-MBX-APIKEY', keys['apiKey']);
-        ourRequest.onload = function(){
-            console.log(ourRequest.responseText);
-        }
-        
-        ourRequest.send();
-        
-    }
+    
     CreateNewOrder(symbol, quanlity, price){
         var url='https://api.binance.com';
         var endpoint = '/api/v3/order';
@@ -91,6 +96,27 @@ export class ExchangeBinance extends Room{
         console.log(Ourl);
         var ourRequest = new XMLHttpRequest();
         ourRequest.open('POST', Ourl, true);
+        ourRequest.setRequestHeader('X-MBX-APIKEY', keys['apiKey']);
+        ourRequest.onload = function(){
+            console.log(ourRequest.responseText);
+        }
+        
+        ourRequest.send();
+    }
+    CanCelOrder(symbol, orderId){
+        var url='https://api.binance.com';
+        var endpoint = '/api/v3/order';
+        var value = 'symbol='+symbol+'&orderId='+orderId+'&recvWindow=20000&timestamp='+Date.now();
+        var keys = {
+            'apiKey' : 'f2bO2GjACQOGM2VLQpjPFbccAjUBZRkA7uNWM40v563mIcofaiIP3qbMTyU7JBH7',
+            'secretKey' : 'ArCBx1AcUw9kJY7RYhnBMGdQDDd4grgkBzLg7D4uW0OypRVn1RN0SYRhZkNl9bER'
+        }
+        var TradeURL = url+endpoint+'?'+value;
+        var signature = crypto.createHmac('sha256', keys['secretKey']).update(value).digest('hex');
+        var Ourl = url + endpoint + '?' + value +  '&signature=' + signature;
+        console.log(Ourl);
+        var ourRequest = new XMLHttpRequest();
+        ourRequest.open('DELETE', Ourl, true);
         ourRequest.setRequestHeader('X-MBX-APIKEY', keys['apiKey']);
         ourRequest.onload = function(){
             console.log(ourRequest.responseText);
@@ -117,13 +143,18 @@ export class ExchangeBinance extends Room{
         
     }
    
-    GetAllPrice(){
+    GetAllPrice(btc){
         var url = "wss://stream.binance.com:9443/ws/!bookTicker";
         const ws2 = new WebSocKet(url);
-        
+        var btc = btc.toUpperCase();
         ws2.on('message', (data: any) => {
-             var all = JSON.parse(data).E;
-             this.broadcast("ticker", data);
+             var s = JSON.parse(data).s;
+             var b = JSON.parse(data).b;
+             
+             if((s.substr(s.length - 3))== btc){
+                this.broadcast("ticker", s +"  " +  b);
+             }
+             
             
         })
         
@@ -135,7 +166,6 @@ export class ExchangeBinance extends Room{
         ws2.on('message', (data: any) => {
              var buy = JSON.parse(data).a;
              this.broadcast("buy", buy);
-             console.log(buy);
             
         })
         
@@ -147,7 +177,6 @@ export class ExchangeBinance extends Room{
         ws2.on('message', (data: any) => {
              var sell = JSON.parse(data).b;
              this.broadcast("sell", sell);
-             console.log(sell);
             
         })
         
@@ -157,7 +186,7 @@ export class ExchangeBinance extends Room{
 
     }
     BookTicker(){
-        var url  = "wss://stream.binance.com:9443/ws/!bookTicker";
+        var url  = "wss://stream.binance.com:9443/ws/!ticker@arr";
         const ws_bookticker = new WebSocKet(url);
         ws_bookticker.on("message", (data: any)=>{
             var s = JSON.parse(data).s;
@@ -176,11 +205,15 @@ export class ExchangeBinance extends Room{
         console.log("Chat room created!", option);
         // this.CreateNewOrder();
         // this.AboutAccount();
-        // this.SellOrder();
+        // this.SellOrder(('trxusdt').toLocaleUpperCase(), '699', '0.01600');
+        this.QueryOrder(('trxusdt').toLocaleUpperCase(), '297597517');
+        // this.CanCelOrder(('trxusdt').toLocaleUpperCase(), '297594816');
         this.GetTradeHistory("adabtc");
-        // this.BookTicker();
-        // this.GetAllPrice();
+        this.GetAllPrice("btc");
+    
+        this.GetOrderBookBUY("adabtc");
         this.GetOrderBookSELL("adabtc");
+        
         this.onMessage("message", (client, message)=>{
             
             
